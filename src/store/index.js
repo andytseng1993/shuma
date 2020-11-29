2d80dd30bf50dd7999db354da5a94feef24cff5a
Andy Tseng <anby1611@gmail.com>
2020-11-29 14:17:52 -0800

    artworks data
    set artworks data in Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    artworks: [],
+    engControl: true,
+    // filter datatype
+    // filterCh: "全部",
+    show: 'All',
+    // light box
+    notes: {},
+    isLightboxOpen: false,
+    now_play_id: 0,
+    scroll: 0,
+    touchDown: 0,
+    touchUp: 0
   },
   mutations: {
+    SETARTWORK (state, artworks) {
+      state.artworks = artworks
+    }
   },
   actions: {
+    GETARTWORK (context) {
+      return axios.get('/artwork.json').then(res => {
+        context.commit('SETARTWORK', res.data)
+      })
+    }
+
   },
   modules: {
   }
