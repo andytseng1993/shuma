ad4c7bf7a86f7867ceae646f92399e0ae73c4857
Andy Tseng <anby1611@gmail.com>
2020-12-05 21:51:01 -0800

    delete typeList
    use props typeMenu to pass the type name from typeList.sort

--- src/components/SelectType/index.vue
+++ src/components/SelectType/index.vue
@@ -11,34 +11,12 @@ export default {
       type: Boolean,
       required: true
     },
-    artworks: {
+    typeMenu: {
       type: Array,
       required: true
     }
   },
   computed: {
-    typeList () {
-      const obj = {
-        sort: [],
-        map: { }
-      }
-      this.artworks.forEach(({ type, title }) => {
-        if (!this.engControl) {
-          if (!obj.map[type[0]]) {
-            obj.sort.push(type[0])
-            obj.map[type[0]] = []
-          }
-          obj.map[type[0]].push(title[0])
-        } else {
-          if (!obj.map[type[1]]) {
-            obj.sort.push(type[1])
-            obj.map[type[1]] = []
-          }
-          obj.map[type[1]].push(title[1])
-        }
-      })
-      return obj
-    },
     selectAll () {
       if (this.engControl) return 'All'
       else return '全部'
