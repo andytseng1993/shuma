84a21a0e4adadc2f22e7d6fc0757af2953cf8bca
Andy Tseng <anby1611@gmail.com>
2021-03-07 15:25:12 -0800

    small adjust about css and html

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -3,11 +3,14 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faHome, faUserCircle, faPalette, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Navigation from '@/components/Navigation/index.vue'
 library.add(faHome, faUserCircle, faPalette, faShoppingCart)
 
 export default {
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    Navigation
+
   },
   computed: {
     engControl: {
