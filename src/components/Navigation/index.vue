c88f1e910d4dd10e9325fe4454eeb8c107004213
Andy Tseng <anby1611@gmail.com>
2021-03-07 15:16:26 -0800

    add nav into the components

---
+++ src/components/Navigation/index.vue
@@ -0,0 +1,23 @@
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faHome, faUserCircle, faPalette, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+library.add(faHome, faUserCircle, faPalette, faShoppingCart)
+
+export default {
+  name: 'Navigation',
+  components: {
+    FontAwesomeIcon
+  },
+  props: {
+    engControl: {
+      type: Boolean,
+      required: true
+    }
+  }
+
+}
+</script>
+<template src="./template.html" />
+<style src ="./style.css" scoped></style>
