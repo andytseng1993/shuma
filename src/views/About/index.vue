435513d9c4208042792fa00646b938736890b845
Andy Tseng <anby1611@gmail.com>
2021-03-13 12:15:55 -0800

    delete unnecessary coding

--- src/views/About/index.vue
+++ src/views/About/index.vue
@@ -1,16 +1,9 @@
 
 <script>
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faHome, faUserCircle, faPalette, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import Navigation from '@/components/Navigation/index.vue'
-library.add(faHome, faUserCircle, faPalette, faShoppingCart)
-
 export default {
   components: {
-    FontAwesomeIcon,
     Navigation
-
   },
   computed: {
     engControl: {
