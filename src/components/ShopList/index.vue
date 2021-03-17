127e5360fade1d68ac2238c50d1f4ed47b960427
Andy Tseng <anby1611@gmail.com>
2021-03-16 17:57:19 -0700

    change template and cass

--- src/components/ShopList/index.vue
+++ src/components/ShopList/index.vue
@@ -2,8 +2,8 @@
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faTimesCircle, faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-library.add(faTimesCircle, faChevronRight, faChevronLeft)
+import { faTimesCircle, faChevronRight, faChevronLeft, faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
+library.add(faTimesCircle, faChevronRight, faChevronLeft, faShoppingBasket)
 
 export default {
   name: 'ShopList',
