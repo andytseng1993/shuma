36172743ba0f605d7401610e164c67f8a6854d6b
Andy Tseng <anby1611@gmail.com>
2021-06-28 15:55:28 -0700

    font

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import 'taipei-sans-tc'
+import 'taipei-sans-tc/dist/Bold/TaipeiSansTCBeta-Bold.css'
 
 Vue.config.productionTip = false
 
