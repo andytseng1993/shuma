1982263eb686aec546406defb7cd933d5245382c
Andy Tseng <anby1611@gmail.com>
2021-07-14 17:25:52 -0700

    fix for 100vh in mobile

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,25 @@
     <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  mounted () {
+    this.resizeEvent()
+  },
+  created () {
+    window.addEventListener('resize', this.resizeEvent)
+  },
+  beforeDestroy () {
+    window.addEventListener('resize', this.resizeEvent)
+  },
+  methods: {
+    resizeEvent () {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }
+  }
+}
+</script>
 <style>
 @font-face {
     font-family: 'Press Start 2P';
@@ -15,7 +33,11 @@
 html,body{
   margin: 0%;
   width:100%;
-  height: 100%;
+}
+body {
+  height: 100vh;
+  /* mobile viewport bug fix */
+  height: calc(var(--vh, 1vh) * 100);
 }
 body.noScroll{
   overflow-y: hidden;
