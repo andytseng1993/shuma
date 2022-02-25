b8248dc4534e2b0822791c36e26138d30a087715
Andy Tseng <anby1611@gmail.com>
2022-02-24 21:38:32 -0800

    home bgImage loading problem

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -13,6 +13,36 @@ export default {
   },
   created () {
     this.$store.dispatch('GET_LANGUAGE')
+  },
+  mounted () {
+    this.canPlay()
+    this.image()
+  },
+  methods: {
+    canPlay () {
+      const video = document.querySelector('video')
+      const videoMobile = document.querySelector('.videoMobile')
+      video.addEventListener('canplay', function () {
+        setTimeout(function () {
+          video.play()
+        }, 1900)
+      })
+      videoMobile.addEventListener('canplay', function () {
+        setTimeout(function () {
+          videoMobile.play()
+        }, 1900)
+      })
+    },
+    image () {
+      const width = window.innerWidth
+      const bgImage = document.querySelector('.bgImage')
+      if (width > 480) bgImage.setAttribute('src', require('../../assets/The-Night-Citizen-big.jpg'))
+      else bgImage.setAttribute('src', require('../../assets/The-Night-Citizen-small.jpg'))
+      bgImage.onload = function () {
+        const preloadBgi = document.querySelector('.preloadBgi')
+        preloadBgi.style.opacity = 0
+      }
+    }
   }
 }
 </script>
